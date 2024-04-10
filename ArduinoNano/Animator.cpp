797b8eed0f55e12ad77dbabcddaf66348c08b4a2
FrankCa99 <131309243+FrankCa99@users.noreply.github.com>
2024-04-10 00:13:20 -0400

    Testing Animator

--- ArduinoNano/Animator.cpp
+++ ArduinoNano/Animator.cpp
@@ -15,7 +15,6 @@ void Animator::begin(CRGB *_led_array, int _start, int _end){
   tail = NULL;
 }
 void Animator::chasing(int len, CRGB _color){
-
   led_array[head] = _color; //turn on front led
   led_array[head - len] = CRGB::Black; // turn off back led
 
